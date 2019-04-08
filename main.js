186fb09652715810827446bc9ea5424e715398c2
Abbey <abigail.hartvigsen@gmail.com>
2019-04-07 20:59:39 -0600

    commented out filtered SWAPI

--- main.js
+++ main.js
@@ -12,114 +12,114 @@
 
 //Is var request, request.open, request.onload, request.send right?
 
-import { films } from './films.js'
-import { people } from './people.js'
-import {planets} from './planets.js'
+// import { films } from './films.js'
+// import { people } from './people.js'
+// import {planets} from './planets.js'
 
 // const intro = document.querySelector('.intro')
 
-films.sort((a,b) => (a.episode_id > b.episode_id) ? 1 : -1 )
+// films.sort((a,b) => (a.episode_id > b.episode_id) ? 1 : -1 )
 
-films.forEach(film => {
-    let tile = document.createElement('div')
+// films.forEach(film => {
+//     let tile = document.createElement('div')
     // does tile need to be changed to title?
 
-    let titleElement = document.createElement('h1')
-  tile.appendChild(titleElement)
+  //   let titleElement = document.createElement('h1')
+  // tile.appendChild(titleElement)
   // does tile need to be changed to title?
-  titleElement.textContent = film.title
+  // titleElement.textContent = film.title
 
-  let crawlElement = document.createElement('div')
-  tile.appendChild(crawlElement)
+  // let crawlElement = document.createElement('div')
+  // tile.appendChild(crawlElement)
   // does tile need to be changed to title?
-  crawlElement.textContent = film.opening_crawl
+//   crawlElement.textContent = film.opening_crawl
 
-  intro.appendChild(tile)
-})
+//   intro.appendChild(tile)
+// })
 
-const allHomeworlds = people.map(person => {
-  let foundWorld = planets.find (planet => {
-    return planet.url === person.homeworld
-  })
-  return {
-    name : person.name,
-    home: foundWorld.name
-  }
-})
+// const allHomeworlds = people.map(person => {
+//   let foundWorld = planets.find (planet => {
+//     return planet.url === person.homeworld
+//   })
+//   return {
+//     name : person.name,
+//     home: foundWorld.name
+//   }
+// })
 
-console.log(allHomeworlds)
+// console.log(allHomeworlds)
 
-const mainContainer = document.createElement('div')
-mainContainer.className='container'
+// const mainContainer = document.createElement('div')
+// mainContainer.className='container'
 
-allHomeworlds.forEach((person) => {
-  let personElement = document.createElement('div')
-  personDiv.classnameName = 'box'
-  let planetElement = document.createElement('p')
+// allHomeworlds.forEach((person) => {
+//   let personElement = document.createElement('div')
+//   personDiv.classnameName = 'box'
+//   let planetElement = document.createElement('p')
 
-  personElement.className = 'box'
-  personElement.textContent = person.name
-  planetElement.textContent = person.home
+//   personElement.className = 'box'
+//   personElement.textContent = person.name
+//   planetElement.textContent = person.home
 
-  personElement.appendChild(planetElement)
-  mainContainer.appendChild(personElement)
-})
+//   personElement.appendChild(planetElement)
+//   mainContainer.appendChild(personElement)
+// })
 
 //do I put mainContainer in the card flip for loop??
 
-document.body.appendChild(mainContainer)
-let filmCharacters = films.filter(newHope => newHope.title == "A New Hope")
-console.log(filmCharacters)
+// document.body.appendChild(mainContainer)
+// let filmCharacters = films.filter(newHope => newHope.title == "A New Hope")
+// console.log(filmCharacters)
 
-let char = 
+// let char = 
 
-console.log(char)
+// console.log(char)
 
 
 
-people.filter((currentValue, index, arr), thisValue)
-let peopleArray = people.filter(peopleFunction1=> peopleFunction1 ["films"] == "https://swapi.co/api/films/1/");
-console.log(peopleArray)
+// people.filter((currentValue, index, arr), thisValue)
+// let peopleArray = people.filter(peopleFunction1=> peopleFunction1 ["films"] == "https://swapi.co/api/films/1/");
+// console.log(peopleArray)
 
-peopleArray.forEach(peopleArray =>{
-  let players =document.createElement('div')
+// peopleArray.forEach(peopleArray =>{
+//   let players =document.createElement('div')
 
-  let players = document.createElement ('h1')
-  players.appendChild
-  console.log(peopleArray)
-} )
+//   let players = document.createElement ('h1')
+//   players.appendChild
+//   console.log(peopleArray)
+// } )
 
-let foo = document.querySelector('.title')
+// let foo = document.querySelector('.title')
 //is this right?
-let bar= document.querySelector('.crawl')
+// let bar= document.querySelector('.crawl')
 //is this right?
 // check both above lines
-titleElement.textContent = films[0].title
-crawlElement.textContent = films[0].opening_crawl
+// titleElement.textContent = films[0].title
+// crawlElement.textContent = films[0].opening_crawl
 // check both above lines
 
-var peopleArray = peopleArray.filter(callback(element[, index[, array]])[, thisArg])
-let peopleArray = people.filter(peopleFunction1=> peopleFunction1 ["films"] == "https://swapi.co/api/films/1/");
-console.log(peopleArray)
+// var peopleArray = peopleArray.filter(callback(element[, index[, array]])[, thisArg])
+// let peopleArray = people.filter(peopleFunction1=> peopleFunction1 ["films"] == "https://swapi.co/api/films/1/");
+// console.log(peopleArray)
 
-try map people through films 
-var new_array = arr.map => (peopleArray) {
-  Return element for new_array
-}[, thisArg])
+// try map people through films 
+// var new_array = arr.map => (peopleArray) {
+//   Return element for new_array
+// }[, thisArg])
 
-peopleArray.map(element => {
-  return peopleArray.length("https://swapi.co/api/films/2/");
-}
+// peopleArray.map(element => {
+//   return peopleArray.length("https://swapi.co/api/films/2/");
+// }
 
-let mainContainer=document.querySelector('.container')
-const peopleArray = people.map((people) => {
-  let newPeopleArray = document.createElement('.div')
-  newPeopleArray.textContent = people
-})
+// let mainContainer=document.querySelector('.container')
+// const peopleArray = people.map((people) => {
+//   let newPeopleArray = document.createElement('.div')
+//   newPeopleArray.textContent = people
+// })
 
-return peopleArray.slice("https://swapi.co/api/films/2/") 
+// return peopleArray.slice("https://swapi.co/api/films/2/") 
 
-console.log(peopleArray)
+// console.log(peopleArray)
 //why is this greyed out?
 
 // peopleArray.forEach(people =>
@@ -146,15 +146,15 @@ console.log(cont)
 for (let i = 0; i < 5; i++) {
 
     ///make cards here add css and the event listener to each card
-    let mainContainer = document.createElement('div')
-    mainContainer (function() {
+    // let mainContainer = document.createElement('div')
+    // mainContainer (function() {
       //is this right?
     console.log("loop")
     let cardCont = document.createElement('div')//create DOM element
     cardCont.addEventListener( 'click', function() {
       cardCont.classList.toggle('is-flipped');
       console.log("click")
-    }
+    
   });
     
     //is this right?
