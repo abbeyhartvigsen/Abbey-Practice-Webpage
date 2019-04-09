eeac5632a4771740943d2017faa10f7e54c8ea5d
Abbey <abigail.hartvigsen@gmail.com>
2019-04-08 22:44:27 -0600

    commenting;experimentingw/imgurl&imgpath

--- main.js
+++ main.js
@@ -13,25 +13,30 @@
 //Is var request, request.open, request.onload, request.send right?
 
 // import { films } from './films.js'
+//does this need to be from '.data/films.js or is this right?
 // import { people } from './people.js'
 // import {planets} from './planets.js'
+//does this need to be from '../data/planets.js ?
 
 // const intro = document.querySelector('.intro')
-
+//does this need to be under films.sort((a,b)=>(a.episode_id > b.episode_id) ? 1 : -1) 
 // films.sort((a,b) => (a.episode_id > b.episode_id) ? 1 : -1 )
+
+//do I need let board = document.createElement('div)
+//do I need board.className = 'board'
 
 // films.forEach(film => {
 //     let tile = document.createElement('div')
-    // does tile need to be changed to title?
+//  does tile need to be changed to title?
 
-  //   let titleElement = document.createElement('h1')
-  // tile.appendChild(titleElement)
-  // does tile need to be changed to title?
-  // titleElement.textContent = film.title
+//   let titleElement = document.createElement('h1')
+// tile.appendChild(titleElement)
+//does tile need to be changed to title?
+// titleElement.textContent = film.title
 
-  // let crawlElement = document.createElement('div')
-  // tile.appendChild(crawlElement)
-  // does tile need to be changed to title?
+// let crawlElement = document.createElement('div')
+// tile.appendChild(crawlElement)
+//does tile need to be changed to title?
 //   crawlElement.textContent = film.opening_crawl
 
 //   intro.appendChild(tile)
@@ -41,9 +46,11 @@
 //   let foundWorld = planets.find (planet => {
 //     return planet.url === person.homeworld
 //   })
+//   let imageURL = getLastNumber(person.url)
 //   return {
 //     name : person.name,
 //     home: foundWorld.name
+//     imagePath: `https://starwars-visualguide.com/assets/img/characters/${imageURL}.jpg`,
 //   }
 // })
 
@@ -65,7 +72,7 @@
 //   mainContainer.appendChild(personElement)
 // })
 
-//do I put mainContainer in the card flip for loop??
+// do I put mainContainer in the card flip for loop??
 
 // document.body.appendChild(mainContainer)
 // let filmCharacters = films.filter(newHope => newHope.title == "A New Hope")
@@ -90,9 +97,9 @@
 // } )
 
 // let foo = document.querySelector('.title')
-//is this right?
+// is this right?
 // let bar= document.querySelector('.crawl')
-//is this right?
+// is this right?
 // check both above lines
 // titleElement.textContent = films[0].title
 // crawlElement.textContent = films[0].opening_crawl
@@ -120,7 +127,7 @@
 // return peopleArray.slice("https://swapi.co/api/films/2/") 
 
 // console.log(peopleArray)
-//why is this greyed out?
+// why is this greyed out?
 
 // peopleArray.forEach(people =>
 
@@ -129,10 +136,10 @@
 // for(var item in obj.options) {
 //   console.log(obj.options[item]);
 // }
-  
+
 // })
 var card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
+card.addEventListener('click', function () {
   card.classList.toggle('is-flipped');
   console.log("click")
 });
@@ -145,35 +152,35 @@ console.log(cont)
 
 for (let i = 0; i < 5; i++) {
 
-    ///make cards here add css and the event listener to each card
-    // let mainContainer = document.createElement('div')
-    // mainContainer (function() {
-      //is this right?
-    console.log("loop")
-    let cardCont = document.createElement('div')//create DOM element
-    cardCont.addEventListener( 'click', function() {
-      cardCont.classList.toggle('is-flipped');
-      console.log("click")
-    
+  ///make cards here add css and the event listener to each card
+  // let mainContainer = document.createElement('div')
+  // mainContainer (function() {
+  //is this right?
+  console.log("loop")
+  let cardCont = document.createElement('div')//create DOM element
+  cardCont.addEventListener('click', function () {
+    cardCont.classList.toggle('is-flipped');
+    console.log("click")
+
   });
-    
-    //is this right?
-    cardCont.className = "card"//create classname
-    //is this right?
-    // cardCont.innerHTML = "card"//filler
-    //is filler a JS thing or is it just an empty thing?
-    cont.appendChild(cardCont)//put element inside the container and in theet HTML
 
-    let front = document.createElement('div')
-    let back = document.createElement('div')
-    front.textContent = "front"
-    back.textContent = "back"
+  //is this right?
+  cardCont.className = "card"//create classname
+  //is this right?
+  // cardCont.innerHTML = "card"//filler
+  //is filler a JS thing or is it just an empty thing?
+  cont.appendChild(cardCont)//put element inside the container and in theet HTML
 
-    front.className = 'card__face card__face--front'
-    back.className = 'card__face card__face--back'
+  let front = document.createElement('div')
+  let back = document.createElement('div')
+  front.textContent = "Front"
+  back.textContent = "Front"
 
-    cardCont.appendChild(front)
-    cardCont.appendChild(back)
+  front.className = 'card__face card__face--front'
+  back.className = 'card__face card__face--back'
+
+  cardCont.appendChild(front)
+  cardCont.appendChild(back)
 
 
 
@@ -290,7 +297,7 @@ for (let i = 0; i < 5; i++) {
 // console.log(peopleArray)
 
 // peopleArray.forEach(people =>
-  
+
 // })
 
 // let dog = {
@@ -315,7 +322,7 @@ for (let i = 0; i < 5; i++) {
 //       return false
 //    }
 //    switching = true
-   
+
 //    $card.toggleClass('is-switched')
 //    window.setTimeout(function () {
 //       $card.children().children().toggleClass('is-active')
